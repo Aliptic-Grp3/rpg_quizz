067fe276478d73c21b1207b90718ca0dcae23052
said8991 <57955763+said8991@users.noreply.github.com>
2019-12-01 19:36:55 +0100

    Add files via upload
    Page de connexion 
    Page d'enregistrement d'un nouvel utilisateur
    Page acceuil

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,25 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\Users;
 
 class AppFixtures extends Fixture
-{
+{   
+	public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
-
+		$user=new Users();
+		$user->setUsername('nour');
+		$password = $this->encoder->encodePassword($user, '1234');
+		$user->setPassword($password);
+		$manager->persist($user);
         $manager->flush();
     }
 }
