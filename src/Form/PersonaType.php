19fa03ab63e1d149801703db248a7e6112e2c60a
nfrugier <nicolas.frugier2@gmail.com>
2019-12-02 11:54:12 +0100

    add persona

---
+++ src/Form/PersonaType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Persona;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('level')
+            ->add('avatar')
+            ->add('sex')
+            ->add('owner')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Persona::class,
+        ]);
+    }
+}
